852f30f3e75d35fe336ccd27ea39b2538fec0320
Scott Morgan <scott.morgan3142@gmail.com>
2020-03-09 21:15:15 +0000

    Dev flask server operational in this commit

--- flask-test.py
+++ flask-test.py
@@ -1,27 +1,29 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Flask, flash, redirect, render_template, request, session, abort, jsonify
 import random
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return '<a href="/run_program">Click here to start the game!</a>'
+def splash():
+    return render_template('splash.html')
 
-@app.route("/run_program/<string:name>/")
-def run_program(name):
-    
-    #insert function here
-    score = random.randint(0,11)
-    
-    string_firstPart = "You scored " + str(score) + ".<br><br>"
-    string_secondPart = '<a href="/">Click here to replay!</a>'
-    string_full = string_firstPart + string_secondPart
-    
-    return render_template('main.html',name=name)
+@app.route("/play/")
+def play():
+    return render_template('play.html')
 
-@app.route("/leaderboard")
-def members():
-    return "Members"
+@app.route("/wait/")
+def wait():
+    from time import sleep
+    sleep(15)
+    return jsonify("We are waiting...")
+
+@app.route("/score/")
+def score():
+        ### begin play funtion ###
+        scores = str(random.randint(0,11))
+        ### end play function
+        
+        return render_template('score.html',scoreValue=scores)
 
 if __name__ == "__main__":
     app.run()

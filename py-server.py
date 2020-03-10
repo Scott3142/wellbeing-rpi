d3ac1757301484eb39adb8c2c6ac05d6f14f913b
Scott Morgan <smorgan@bridgend.ac.uk>
2020-03-10 15:03:30 +0000

    Updates script for RPi

--- py-server.py
+++ py-server.py
@@ -1,7 +1,7 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort, jsonify
 import json
 import random
-#from gpiozero import LED,Button
+from gpiozero import LED,Button
 from time import time,sleep
 
 app = Flask(__name__)
@@ -19,8 +19,85 @@ def game_playing():
     
     ### Put code here ###
 
-    current_score = str(random.randint(0,11))
-    sleep(5)
+    led1 = LED(3)
+    button1 = Button(27)
+    led2 = LED(21)
+    button2 = Button(13)
+    led3 = LED(10)
+    button3 = Button(7)
+    led4 = LED(15)
+    button4 = Button(5)
+    led5 = LED(4)
+    button5 = Button(2)
+    my_list = ['led1', 'led2', 'led3', 'led4', 'led5']
+    
+    reaction_times = []
+    a = 100
+    b = 75
+    c = 50
+    d = 25
+    e = 10
+    
+    score = 0
+    number_of_hits = 10
+    
+#   while True:
+#       
+#       led1.on()
+#       button1.wait_for_press()
+#       led1.off()
+    
+    for index in range(0,number_of_hits):
+        
+        sleep(random.randint(1,5))
+    
+        start_time = time()
+    
+        choice = random.choice (my_list)
+    
+        if choice == 'led1':
+            led1.on()
+            button1.wait_for_press()
+            led1.off()
+    
+        elif choice == 'led2':
+            led2.on()
+            button2.wait_for_press()
+            led2.off()
+            
+        elif choice == 'led3':
+            led3.on()
+            button3.wait_for_press()
+            led3.off()
+            
+        elif choice == 'led4':
+            led4.on()
+            button4.wait_for_press()
+            led4.off()
+            
+        elif choice == 'led5':
+            led5.on()
+            button5.wait_for_press()
+            led5.off()
+            
+        reaction_time = time() - start_time
+    
+        if reaction_time <2:
+            score += a
+        if reaction_time >2 and reaction_time <2.5:
+            score += b
+        if reaction_time >2.5 and reaction_time <2.9:
+            score += c
+        if reaction_time >3 and reaction_time <5:
+            score += d
+        if reaction_time >5:
+            score += e
+    
+#    print(score)
+#    sleep (5)
+#    score = 0
+
+    current_score = score
 
     ### End code ###
 
